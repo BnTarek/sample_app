b01915ce5573e9593489a4f87b193b70d0d85892
Mohammad Tarek <mohammed.tarek@espace.com.eg>
2015-07-05 16:33:15 +0200

    Finish user edit, update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 User.create!(name:  "Mohammad Tarek",
              email: "uname5000@gmail.com",
              password:              "12345678",
-             password_confirmation: "12345678")
+             password_confirmation: "12345678",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
