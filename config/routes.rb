925495b17441515c3dfac7aa28584e30ff622286
Mohammad Tarek <mohammed.tarek@espace.com.eg>
2015-07-06 18:37:50 +0200

    Add password resets and email confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   root             'static_pages#home'
   
   get 'help'    => 'static_pages#help'
@@ -13,9 +13,9 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
 
   resources :users
-
   resources :account_activations, only: [:edit]
-  
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
