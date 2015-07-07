39b3dbde048cb153678695bd0ae0a439761bd917
Mohammad Tarek <mohammed.tarek@espace.com.eg>
2015-07-07 11:52:21 +0200

    Change smtp email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,13 +78,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "mallaky.herokuapp.com" }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'mallaky.herokuapp.com',
-    user_name:            'uname5005@gmail.com',
-    password:             '123698745ABC',
+    user_name:            '',
+    password:             '',
     authentication:       'plain',
     enable_starttls_auto: true  }
-  config.action_mailer.default_url_options = { :host => "mallaky.herokuapp.com" }
 end
